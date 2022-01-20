6ee72dd29167abd61f6f2833aba4602697c15122
VrgX0 <e.kenner@gmx.net>
2022-01-20 16:30:12 +0100

    initial commit

---
+++ src/denseFlow.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <opencv2/core.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/videoio.hpp>
+#include <opencv2/video.hpp>
+#include <chrono>
+
+using namespace cv;
+using namespace std;
+
+int main()
+{
+    //VideoCapture capture(R"(D:\MPI-Sintel-complete\training\clean\bamboo_1\frame_%04d.png)");
+    VideoCapture capture(samples::findFile("vtest.avi"));
+    if (!capture.isOpened()){
+        //error in opening the video input
+        cerr << "Unable to open file!" << endl;
+        return 0;
+    }
+    //initialise first frame
+    Mat frame1, prvs;
+    capture >> frame1;
+    //convert into Grayscale picture
+    cvtColor(frame1, prvs, COLOR_BGR2GRAY);
+    auto startLoop = chrono::high_resolution_clock::now();
+    while(true){
+        //initialize second frame
+        Mat frame2, next;
+        capture >> frame2;
+        //check if sequence ended
+        if (frame2.empty())
+            break;
+        //convert into Grayscale picture
+        cvtColor(frame2, next, COLOR_BGR2GRAY);
+        Mat flow(prvs.size(), CV_32FC2);
+        auto start = chrono::high_resolution_clock::now();
+        calcOpticalFlowFarneback(prvs, next, flow, 0.5, 3, 15, 3, 5, 1.2, 0);
+        auto end = chrono::high_resolution_clock::now();
+        // visualization
+        Mat flow_parts[2];
+        //split flow into multiple
+        split(flow, flow_parts);
+        Mat magnitude, angle, magn_norm;
+        //calculate magnitude and angles
+        cartToPolar(flow_parts[0], flow_parts[1], magnitude, angle, true);
+        normalize(magnitude, magn_norm, 0.0f, 1.0f, NORM_MINMAX);
+        angle *= ((1.f / 360.f) * (180.f / 255.f));
+
+        //build hsv image
+        Mat _hsv[3], hsv, hsv8, bgr;
+        _hsv[0] = angle;
+        _hsv[1] = Mat::ones(angle.size(), CV_32F);
+        _hsv[2] = magn_norm;
+        merge(_hsv, 3, hsv);
+        hsv.convertTo(hsv8, CV_8U, 255.0);
+        cvtColor(hsv8, bgr, COLOR_HSV2BGR);
+        imshow("frame2", bgr);
+        int keyboard = waitKey(30);
+        if (keyboard == 'q' || keyboard == 27)
+            break;
+        prvs = next;
+        cout << chrono::duration_cast<chrono::duration<double, milli>>(end - start).count() << endl;
+    }
+    auto endLoop = chrono::high_resolution_clock::now();
+    //cout << chrono::duration_cast<chrono::duration<double, milli>>(endLoop - startLoop).count() << endl;
+}
+
