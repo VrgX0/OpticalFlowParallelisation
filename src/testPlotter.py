311cd6a1ea7d3a3d7cc8e92995b74d6b2d5cd162
VrgX0 <e.kenner@gmx.net>
2022-02-19 14:14:11 +0100

    add plotter

--- src/testPlotter.py
+++ src/testPlotter.py
@@ -3,32 +3,47 @@ import numpy as np
 import argparse
 
 from numpy import double
+PolyOvTime = 0
+UpdateOvTime = 0
+FlowOvTime = 0
+OvTime = 0
+count = 0
 
-'''
-x = [0, 1, 1, 2, 2, 3, 3, 4]
-y = values
-#y2 = [0, 0.2181, 0.2567, 0.2877, 0.4586, 0.6546]
-plt.plot(x, y, label='sequenziel', marker='x')  # Plot some data on the (implicit) axes.
-#plt.plot(x, y2, label='parallel', marker='x')  # etc.
-#plt.plot(x, x**3, label='cubic')
-#plt.axis([0, 2.00, 0, 8.00])
-plt.grid(True)
-plt.xlabel('Schritte der Ausführung')
-plt.ylabel('Zeit in ms')
-plt.title("Example Plot")
-plt.legend()
-# save plot as svg
-plt.savefig('test_plot.svg')
-plt.clf()
-'''
-seqMS = 0
-parMS = 0
+file = open("example.txt", "r")
+for line in file.readlines():
+    if line.startswith(" FarnebackP"):
+        PolyOvTime = PolyOvTime + double(line.split()[1])
+    elif line.startswith(" FarnebackU"):
+        UpdateOvTime = UpdateOvTime + double(line.split()[1])
+    elif line.startswith(" FarnebackF"):
+        FlowOvTime = FlowOvTime + double(line.split()[1])
+    elif line.startswith("Overall"):
+        OvTime = OvTime + double(line.split()[4])
+        count = count + 1
 
-names = ['sequenziel',  'std::par']
-heights = [seqMS, parMS]
-plt.bar(names, heights)
+PolyAvTime = PolyOvTime / (count * 8)
+UpdateAvTime = UpdateOvTime / (count * 4)
+FlowAvTime = FlowOvTime / (count * 12)
+AvTime = OvTime / count
+
+PolyD = (PolyAvTime * 8) / AvTime
+UpdateD = (UpdateAvTime * 4) / AvTime
+FlowD = (FlowAvTime * 12) / AvTime
+OtherD = (AvTime - (PolyAvTime*8 + UpdateAvTime*4 + FlowAvTime*12)) / AvTime
+
+names = ['FarnebackPolyExp', 'FarnebackUpdateMatrices', 'FarnebackUpdateFlow_Blur']
+heights = [PolyAvTime, UpdateAvTime, FlowAvTime]
+labels = ['FarnebackPolyExp', 'FarnebackUpdateMatrices', 'FarnebackUpdateFlow_Blur', 'Other']
+sizes = [PolyD, UpdateD, FlowD, OtherD]
+plt.figure(figsize=(7, 5))
+plt.bar(names, heights, width=0.5)
 plt.ylabel('Zeit in ms')
-plt.title("Execution Time")
+plt.title("Durchschnittliche Ausführungszeit")
 plt.savefig('test_plot_bar.svg')
+plt.clf()
+plt.figure(figsize=(8.5, 5))
+plt.pie(sizes, explode=(0.1, 0, 0, 0), labels=labels, autopct='%.2f%%', shadow=True, startangle=90)
+plt.axis('equal')
+plt.savefig('test_plot_pie.svg')
 
 
