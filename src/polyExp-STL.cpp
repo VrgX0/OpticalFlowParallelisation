0d21a4f45f3c482a786efcbceb14e7738884c82d
VrgX0 <e.kenner@gmx.net>
2022-02-26 16:27:53 +0100

    testing polyExp-STL.cpp

--- src/polyExp-STL.cpp
+++ src/polyExp-STL.cpp
@@ -4,8 +4,8 @@
 #include <algorithm>
 #include <execution>
 
-const size_t testWidth = 10;
-const size_t testHeight = 10;
+const size_t testWidth = 100;
+const size_t testHeight = 100;
 int n = 5;
 
 
@@ -16,9 +16,9 @@ static void FarnebackPolyExpPPstl(const std::vector<float>& src, std::vector<flo
     std::vector<float> kbuf (n*6 + 3, 0.23);
     double ig11 = 0.12, ig03 = 0.14, ig33 = 0.13, ig55 = 1.23;
 
-    auto src_ptr = src.data();
+    auto src_ptr = &src[0];
     auto dst_ptr = dst.data();
-    std::for_each(std::execution::par_unseq, src_ptr,src_ptr + (width * height),[=](auto &pix){
+    std::for_each(std::execution::par_unseq, src.begin(), src.end(),[=](auto &pix){
         int xgOff = n + n*2 +1;
         int xxgOff = xgOff +n*2;
         float g0 = kbuf[0+n];
@@ -64,6 +64,73 @@ static void FarnebackPolyExpPPstl(const std::vector<float>& src, std::vector<flo
     });
 }
 
+static void FarnebackPolyExpPPstl2(const std::vector<float>& src, std::vector<float>& dst)
+{
+    int width = testWidth;
+    int height = testHeight;
+    std::vector<float> kbuf (n*6 + 3, 0.23);
+    double ig11 = 0.12, ig03 = 0.14, ig33 = 0.13, ig55 = 1.23;
+
+    auto src_ptr = &src[0];
+    auto dst_ptr = dst.data();
+    std::for_each(std::execution::par_unseq, src.begin(), src.end(),[=](auto &pix){
+        int xgOff = n + n*2 +1;
+        int xxgOff = xgOff +n*2;
+        float g0 = kbuf[0+n];
+        float rBuf;
+        float xrBuf;
+        float xxrBuf;
+
+        double b1 = 0, b2 = 0, b3 = 0, b4 = 0, b5 = 0, b6 = 0;
+
+        auto index = &pix - src_ptr;
+        int x = index % width;
+        int y = index / width;
+
+        for( int a = 0; a < 2*n+1; a++){
+            int neighX = std::max(x + a-n, 0);
+            neighX = std::min(neighX, width-1);
+            rBuf = src_ptr[neighX + y * width] * g0;
+            for(int b = 1; b <= n; b++) {
+                int neighY0 = std::max((y - b) * width, 0);
+                int neighY1 = std::min((y + b) * width, (height - 1) * width);
+                rBuf += (src_ptr[neighX + neighY0] + src_ptr[neighX + neighY1]) * kbuf[b+n];
+                xrBuf += (src_ptr[neighX + neighY1] - src_ptr[neighX + neighY0]) * kbuf[b+xgOff];
+                xxrBuf += (src_ptr[neighX + neighY0] + src_ptr[neighX + neighY1]) * kbuf[b+xxgOff];
+            }
+            float g = kbuf[abs(a-n) + n];
+            float xg = kbuf[abs(a-n) + xgOff];
+            float xxg = kbuf[abs(a-n) + xxgOff];
+            if(a == n){
+                b1 += rBuf * g;
+                b3 += xrBuf * g;
+                b5 += xxrBuf * g;
+            } else if(a > n){
+                b1 += rBuf * g;
+                b2 += rBuf * xg;
+                b3 += xrBuf * g;
+                b4 += rBuf * xxg;
+                b5 += xxrBuf * g;
+                b6 += xrBuf * xg;
+            } else {
+                b1 += rBuf * g;
+                b2 -= rBuf * xg;
+                b3 += xrBuf * g;
+                b4 += rBuf * xxg;
+                b5 += xxrBuf * g;
+                b6 -= xrBuf * xg;
+            }
+        }
+        /*
+        int pixel = x+y*width;
+        dst_ptr[pixel*5+1] = (float)(b2*ig11);
+        dst_ptr[pixel*5] = (float)(b3*ig11);
+        dst_ptr[pixel*5+3] = (float)(b1*ig03 + b4*ig33);
+        dst_ptr[pixel*5+2] = (float)(b1*ig03 + b5*ig33);
+        dst_ptr[pixel*5+4] = (float)(b6*ig55);
+        */
+    });
+}
 
 int main() {
     std::vector<float> src (testHeight * testWidth);
@@ -71,8 +138,9 @@ int main() {
         i = 5.f;
     }
     std::vector<float> dst ((testHeight*testWidth)*5);
+    std::transform(std::execution::par_unseq, src.begin(), src.end(), src.begin(), [](auto a){return a*2;});
     std::cout << "begin" << std::endl;
-    FarnebackPolyExpPPstl(src, dst);
+    FarnebackPolyExpPPstl2(src, dst);
     std::cout << "end" << std::endl;
     return 0;
 }
