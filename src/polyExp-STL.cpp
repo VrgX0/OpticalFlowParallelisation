4704520db18ec806d77086de408e4eb5428c6a43
VrgX0 <e.kenner@gmx.net>
2022-02-26 19:22:57 +0100

    testing polyExp-STL.cpp

--- src/polyExp-STL.cpp
+++ src/polyExp-STL.cpp
@@ -20,7 +20,7 @@ static void FarnebackPolyExpPPstl(const std::vector<float>& src, std::vector<flo
     auto dst_ptr = dst.data();
     std::for_each(std::execution::par_unseq, src.begin(), src.end(),[=](auto &pix){
         int xgOff = n + n*2 +1;
-        int xxgOff = xgOff +n*2;
+        int xxgOff = xgOff + n*2 + 1;
         float g0 = kbuf[0+n];
         std::vector<float> rBuf((2 * n + 1)*3,0.f);
         int offset = 2*n+1;
@@ -64,7 +64,7 @@ static void FarnebackPolyExpPPstl(const std::vector<float>& src, std::vector<flo
     });
 }
 
-static void FarnebackPolyExpPPstl2(const std::vector<float>& src, std::vector<float>& dst)
+static void FarnebackPolyExpPPstl2(const std::vector<float>& src, std::vector<std::vector<float>>& dst)
 {
     int width = testWidth;
     int height = testHeight;
@@ -72,10 +72,10 @@ static void FarnebackPolyExpPPstl2(const std::vector<float>& src, std::vector<fl
     double ig11 = 0.12, ig03 = 0.14, ig33 = 0.13, ig55 = 1.23;
 
     auto src_ptr = &src[0];
-    auto dst_ptr = dst.data();
-    std::for_each(std::execution::par_unseq, src.begin(), src.end(),[=](auto &pix){
+    //auto dst_ptr = dst.data();
+    std::transform(std::execution::par_unseq, src.begin(), src.end(),dst.begin(),[=](auto &pix){
         int xgOff = n + n*2 +1;
-        int xxgOff = xgOff +n*2;
+        int xxgOff = xgOff + n*2 +1;
         float g0 = kbuf[0+n];
         float rBuf;
         float xrBuf;
@@ -91,6 +91,8 @@ static void FarnebackPolyExpPPstl2(const std::vector<float>& src, std::vector<fl
             int neighX = std::max(x + a-n, 0);
             neighX = std::min(neighX, width-1);
             rBuf = src_ptr[neighX + y * width] * g0;
+            xrBuf = 0;
+            xxrBuf = 0;
             for(int b = 1; b <= n; b++) {
                 int neighY0 = std::max((y - b) * width, 0);
                 int neighY1 = std::min((y + b) * width, (height - 1) * width);
@@ -121,14 +123,10 @@ static void FarnebackPolyExpPPstl2(const std::vector<float>& src, std::vector<fl
                 b6 -= xrBuf * xg;
             }
         }
-        /*
-        int pixel = x+y*width;
-        dst_ptr[pixel*5+1] = (float)(b2*ig11);
-        dst_ptr[pixel*5] = (float)(b3*ig11);
-        dst_ptr[pixel*5+3] = (float)(b1*ig03 + b4*ig33);
-        dst_ptr[pixel*5+2] = (float)(b1*ig03 + b5*ig33);
-        dst_ptr[pixel*5+4] = (float)(b6*ig55);
-        */
+
+        return std::vector<float> {(float)(b3*ig11), (float)(b2*ig11),
+                                   (float)(b1*ig03 + b5*ig33),
+                                   (float)(b1*ig03 + b4*ig33), (float)(b6*ig55) };
     });
 }
 
@@ -138,9 +136,10 @@ int main() {
         i = 5.f;
     }
     std::vector<float> dst ((testHeight*testWidth)*5);
+    std::vector<std::vector<float>> Vdst (testWidth*testHeight);
     std::transform(std::execution::par_unseq, src.begin(), src.end(), src.begin(), [](auto a){return a*2;});
     std::cout << "begin" << std::endl;
-    FarnebackPolyExpPPstl2(src, dst);
+    FarnebackPolyExpPPstl2(src, Vdst);
     std::cout << "end" << std::endl;
     return 0;
 }
